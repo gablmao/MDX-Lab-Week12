7e3114e672c6c73147f5e392787c6054365193f3
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-12-13 14:23:45 +0000

    lol

---
+++ notes.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "lol";
+
+    return 0;
+}
+
+/*
+WEEK 12: ABSTRACT DATA TYPES WHAAAAAAAAAAAAAAAAAAAAAAAT
+
+> a data type whose representation is hidden
+    > focus on operations, ignore the data representation
+
+
+> C++ ADTs
+    > string complex valarray
+        > collections:
+        - array
+        - vector
+        - list
+        - stack
+        - queue
+        - set
+        - map
+
+        > others:
+        - date
+        - counter
+        - stopwatch
+
+> member functions are the API of an ADT
+    > encapsulate data types within implementation
+        > avoid clients relying on implementation details
+        > allow change in implementation
+
+
+> templates
+    > e.g., vectors (std::vector<int> name) is a template class
+    > 
+*/
